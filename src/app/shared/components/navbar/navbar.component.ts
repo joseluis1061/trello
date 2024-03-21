63b2547d98a26a3c2ddd55288f3bd06d4a434d48
José Luis Zúñiga <jose@tangibledesign.co>
2024-03-21 12:05:07 -0500

    Se agregan los iconos de alerta y notificación a navBar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,14 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BtnComponent } from '../btn/btn.component';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faClose, faBell, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule, OverlayModule, BtnComponent],
+  imports: [CommonModule, OverlayModule, FontAwesomeModule, BtnComponent],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
   activeMenu: boolean = false;
+  faClose = faClose;
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
 
 }
