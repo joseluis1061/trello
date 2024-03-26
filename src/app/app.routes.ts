8f09c109234fdfd8a36b26cd169f9aeb158b4e8d
José Luis Zúñiga <jose@tangibledesign.co>
2024-03-26 12:05:49 -0500

    Se crea tabla a partir de products()

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'scroll'
+    redirectTo: 'table'
   },
   {
     path: 'login',
--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { ProductsService } from '../../service/products.service';
+import { CdkTableModule } from '@angular/cdk/table';
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [CdkTableModule],
   templateUrl: './table.component.html'
 })
-export class TableComponent {
+
+
+export class TableComponent implements OnInit {
+
+  products = signal<Product[]>([])
+
+  private productsService = inject(ProductsService);
+
+  ngOnInit(): void {
+    this.productsService.getAllProducts().subscribe({
+      next: ((respone) => {this.products.set(respone)})
+    })
+  }
+
+
 
 }
